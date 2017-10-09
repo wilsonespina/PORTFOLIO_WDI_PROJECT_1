64c7944a5e27c8a14efae50386bc916fd349b928
wilsonespina <wilson.espina@gmail.com>
2017-10-09 23:05:49 +0100

    creating walls

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ $(() => {
   //Assign key bindings to arrow keys to make digger move around the grid
 
   const grid = ['b0','b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12','b13','b14','b15','b16','b17','b18','b19','b20','b21','b22','b23','b24','b25','b26','b27','b28','b29','b30','b31','b32','b33','b34','b35','b36','b37','b38','b39','b40','b41','b42','b43','b44','b45','b46','b47','b48','b49','b50','b51','b52','b53','b54','b55','b56','b57','b58','b59','b60','b61','b62','b63','b63'];
-  const gameString1 = ['w','w','w','w','w','w','w','w','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','w','w','w','w','w','w','w','w'];
+  const gameString1 = ['w','w','w','w','w','w','w','w','w','c','c','c','c','c','c','w','w','c','c','c','c','c','c','w','w','c','c','c','c','c','c','w','w','c','c','c','c','c','c','w','w','c','c','c','c','c','c','w','w','c','c','c','c','c','c','w','w','w','w','w','w','w','w','w'];
   const $li = $('.li');
   const $ul = $('.ul');
   const digger = $('<img id ="digger" src="images/digger.png" alt="digger image" />');
@@ -26,25 +26,34 @@ $(() => {
   function init (){
     //start potioning
     loadBoard();
+    // walls();
     $playButton.on('click', function() {
       $beginningSquare.append(digger);
     });
     moveDigger();
     spawn();
 
-    //CREATE OBSTACLES
+    //CREATE BOARD
     function loadBoard(){
       // const gameString = [];
       for (let i = 0; i < grid.length; i++) {
         $('#grid li:nth-child(' + i + ')').html(gameString1[i-1]);
       }
-      if ($ul.includes('w')) {
-        $('#grid li:nth-child(' + i + ')').append(block);
-      } else {
-        return;
-      }
     }
 
+    //CREATE WALLS
+    // function walls(){
+    //   for (let i = 0; i < grid.length; i++) {
+    //     if ($li.text() == gameString1[i]) {
+    //       console.log(gameString1[i]);
+    //     // $('#grid li:nth-child(' + i + ')').append(block);
+    //     } else {
+    //       return;
+    //     }
+    //   }
+    // }
+
+    
     // MOVE DIGGER
     function moveDigger(){
       let n = startSquare;
@@ -53,7 +62,7 @@ $(() => {
       $(document).keydown(function(e) {
         switch(e.which) {
           case 37: // left
-            if (n >= 2 && n <= 64 && n !== 1 && n !== 9 && n !== 17 && n !== 25 && n !== 33 && n !== 41 && n !== 49 && n !== 57) {
+            if (n >= 2 && n <= 64 && n !== 1 && n !== 9 && n !== 17 && n !== 25 && n !== 33 && n !== 41 && n !== 49 && n !== 57){
               (n -= 1);
               $('#grid li:nth-child(' + n + ')').append(digger);
             } else {
