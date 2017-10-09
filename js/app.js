f888ab3092b73a669017caa0edf6f1e4f2cbd33d
wilsonespina <wilson.espina@gmail.com>
2017-10-09 22:34:33 +0100

    creating walls

--- js/app.js
+++ js/app.js
@@ -10,16 +10,18 @@ $(() => {
   //Assign key bindings to arrow keys to make digger move around the grid
 
   const grid = ['b0','b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12','b13','b14','b15','b16','b17','b18','b19','b20','b21','b22','b23','b24','b25','b26','b27','b28','b29','b30','b31','b32','b33','b34','b35','b36','b37','b38','b39','b40','b41','b42','b43','b44','b45','b46','b47','b48','b49','b50','b51','b52','b53','b54','b55','b56','b57','b58','b59','b60','b61','b62','b63','b63'];
+  const gameString1 = ['w','w','w','w','w','w','w','w','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','w','w','w','w','w','w','w','w'];
   const $li = $('.li');
-  const digger = $('<img id ="digger" src="images/digger.png" alt="digger image" />'); //does it need te be .digger?
+  const $ul = $('.ul');
+  const digger = $('<img id ="digger" src="images/digger.png" alt="digger image" />');
+  const block = $('<img id ="block" src="images/digger.png" alt="digger image" />');
   const $playButton = $('#play-button');
   const gridWidth = 8; //UPDATE WITH BIGGER GRID
-  const startSquare = 36; //UPDATE with new game?
+  const startSquare = 42; //UPDATE with new game?
   const $beginningSquare = $('#grid li:nth-child(' + startSquare + ')');
-  const gameString1 = ['w','w','w','w','w','w','w','w','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','w','w','w','w','w','w','w','w'];
+
 
   init();
-
 
   function init (){
     //start potioning
@@ -32,9 +34,15 @@ $(() => {
 
     //CREATE OBSTACLES
     function loadBoard(){
-
-
-
+      // const gameString = [];
+      for (let i = 0; i < grid.length; i++) {
+        $('#grid li:nth-child(' + i + ')').html(gameString1[i-1]);
+      }
+      if ($ul.includes('w')) {
+        $('#grid li:nth-child(' + i + ')').append(block);
+      } else {
+        return;
+      }
     }
 
     // MOVE DIGGER
