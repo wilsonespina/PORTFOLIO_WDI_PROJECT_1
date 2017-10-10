0dd32706f6a69b3fe50a097d68bcbb53fc99a64b
wilsonespina <wilson.espina@gmail.com>
2017-10-10 20:57:00 +0100

    mvp

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(() => {
 
   console.log('loaded');
@@ -11,7 +10,7 @@ $(() => {
   //Assign key bindings to arrow keys to make digger move around the grid
 
   const grid = ['b0','b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12','b13','b14','b15','b16','b17','b18','b19','b20','b21','b22','b23','b24','b25','b26','b27','b28','b29','b30','b31','b32','b33','b34','b35','b36','b37','b38','b39','b40','b41','b42','b43','b44','b45','b46','b47','b48','b49','b50','b51','b52','b53','b54','b55','b56','b57','b58','b59','b60','b61','b62','b63','b64'];
-  const gameString1 = ['w','w','w','w','w','w','w','w','w','c','w','w','c','w','c','w','w','c','c','c','c','c','c','w','w','c','w','c','w','c','w','w','w','w','c','c','w','c','c','w','w','w','c','w','c','c','w','w','w','c','c','c','c','c','w','w','w','w','w','w','w','w','w','w'];
+  const gameString1 = ['w','w','w','w','w','w','w','w','w','c','w','w','c','w','c','w','w','c','c','c','c','c','c','w','w','c','w','c','w','c','w','w','w','w','c','c','w','c','c','w','w','w','c','w','c','c','g','g','w','c','c','c','c','c','g','g','w','w','w','w','w','w','g','g'];
   const $li = $('.li');
   const $ul = $('.ul');
   const digger = $('<img id ="digger" src="images/digger.png" alt="digger image" />');
@@ -19,9 +18,11 @@ $(() => {
   const ruby = $('<img id ="ruby" src="images/ruby.png" alt="ruby image" />');
   const $playButton = $('#play-button');
   const $resetButton = $('#reset-button');
-  // const gridWidth = 8; //UPDATE WITH BIGGER GRID
+  const gridWidth = 8; //UPDATE WITH BIGGER GRID
   const startSquare = 10; //UPDATE with new game?
   let $beginningSquare = null;
+  let level = null; //change according to difficulty
+
   let score1 = 0;
   let timer = 60;
   const $score = $('#score');
@@ -54,76 +55,58 @@ $(() => {
       let n = startSquare;
       let score = 0;
 
+      function ruby(){
+        if (($('#grid li:nth-child(' + n + ')').attr('class')).includes('ruby')) {
+          $('#grid li:nth-child(' + n + ') img').remove();
+          const $remove = $('#grid li:nth-child(' + n + ')');
+          $($remove).removeClass('ruby');
+          spawn();
+          score++;
+          $('#grid li:nth-child(' + n + ')').append(digger);
+        }
+      }
+
       $(document).keydown(function(e) {
         switch(e.which) {
           case 37: // left
-            if (n >= 2 && n <= 64 && n !== 1 && n !== 9 && n !== 17 && n !== 25 && n !== 33 && n !== 41 && n !== 49 && n !== 57 && gameString1[n-2] !== 'w'){
+            if (n >= 2 && n <= grid.length && n !== 1 && n !== 9 && n !== 17 && n !== 25 && n !== 33 && n !== 41 && n !== 49 && n !== 57 && gameString1[n-2] !== 'w'){
               (n -= 1);
               $('#grid li:nth-child(' + n + ')').append(digger);
-              if (($('#grid li:nth-child(' + n + ')').attr('class')).includes('ruby')) {
-                $('#grid li:nth-child(' + n + ') img').remove();
-                const $remove = $('#grid li:nth-child(' + n + ')');
-                $($remove).removeClass('ruby');
-                spawn();
-                score++;
+              ruby();
 console.log(score);
-                $('#grid li:nth-child(' + n + ')').append(digger);
-              }
             } else {
               return;
             }
             break;
 
           case 38: // up
-            if (n >= 9 && n <= 64 && gameString1[n-9] !== 'w') {
-              (n -= 8);
+            if (n >= 9 && n <= grid.length && gameString1[n-9] !== 'w') {
+              (n -= gridWidth);
               $('#grid li:nth-child(' + n + ')').append(digger);
-              if (($('#grid li:nth-child(' + n + ')').attr('class')).includes('ruby')) {
-                $('#grid li:nth-child(' + n + ') img').remove();
-                const $remove = $('#grid li:nth-child(' + n + ')');
-                $($remove).removeClass('ruby');
-                spawn();
-                score++;
+              ruby();
 console.log(score);
-                $('#grid li:nth-child(' + n + ')').append(digger);
-
-              }
             } else {
               return;
             }
             break;
 
           case 39: // right
-            if (n >= 1 && n <= 63 && n !== 8 && n !== 16 && n !== 24 && n !== 32 && n !== 40 && n !== 48 && n !== 56 && n !== 64 && gameString1[n] !== 'w') {
+            if (n >= 1 && n <= (grid.length-1) && n !== 8 && n !== 16 && n !== 24 && n !== 32 && n !== 40 && n !== 48 && n !== 56 && n !== 64 && gameString1[n] !== 'w') {
               (n += 1);
               $('#grid li:nth-child(' + n + ')').append(digger);
-              if (($('#grid li:nth-child(' + n + ')').attr('class')).includes('ruby')) {
-                $('#grid li:nth-child(' + n + ') img').remove();
-                const $remove = $('#grid li:nth-child(' + n + ')');
-                $($remove).removeClass('ruby');
-                spawn();
-                score++;
+              ruby();
 console.log(score);
-                $('#grid li:nth-child(' + n + ')').append(digger);
-              }
             } else {
               return;
             }
             break;
 
           case 40: // down
-            if (n >= 1 && n <= 56 && gameString1[n+7] !== 'w') {
-              (n += 8);
+            if (n >= 1 && n <= (grid.length-gridWidth) && gameString1[n+7] !== 'w') {
+              (n += gridWidth);
               $('#grid li:nth-child(' + n + ')').append(digger);
-              if (($('#grid li:nth-child(' + n + ')').attr('class')).includes('ruby')) {
-                $('#grid li:nth-child(' + n + ') img').remove();
-                const $remove = $('#grid li:nth-child(' + n + ')');
-                $($remove).removeClass('ruby');
-                spawn();
-                score++;
+              ruby();
 console.log(score);
-                $('#grid li:nth-child(' + n + ')').append(digger);
-              }
             } else {
               return;
             }
@@ -132,9 +115,6 @@ console.log(score);
         }
         e.preventDefault(); // prevent the default action (scroll / move caret)
       });
-
-
-
     }
 
     //SPAWN RANDOM OBJECT
@@ -142,7 +122,6 @@ console.log(score);
       const $clear = $('.c');
       const $randomSpawn = Math.floor(Math.random() * ($($clear).length));
       const $randomLocation = $clear[$randomSpawn];
-// console.log($randomLocation);
       $($randomLocation).addClass('ruby').append(ruby);
     }
 
@@ -156,15 +135,14 @@ console.log(score);
       // if (){
       //
       // }
-
     }
 
 
     // SCOREBOARD
     function scoreboard(){
-      let score = 0;
-      if (score >= 0) $score.html(score);
-      $input.val('');
+    //   let score = 0;
+    //   if (score >= 0) $score.html(score);
+    //   $input.val('');
     }
 
     // RESET GAME
