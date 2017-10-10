f68d8ff7ddc36f4e7599faad772d4c211ea59233
wilsonespina <wilson.espina@gmail.com>
2017-10-10 16:57:54 +0100

    adding spawn object

--- js/app.js
+++ js/app.js
@@ -10,17 +10,18 @@ $(() => {
   //Assign key bindings to arrow keys to make digger move around the grid
 
   const grid = ['b0','b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12','b13','b14','b15','b16','b17','b18','b19','b20','b21','b22','b23','b24','b25','b26','b27','b28','b29','b30','b31','b32','b33','b34','b35','b36','b37','b38','b39','b40','b41','b42','b43','b44','b45','b46','b47','b48','b49','b50','b51','b52','b53','b54','b55','b56','b57','b58','b59','b60','b61','b62','b63','b64'];
-  const gameString1 = ['w','w','w','w','w','w','w','w','w','c','c','c','c','c','c','w','w','c','c','c','c','c','c','w','w','c','c','c','w','c','w','w','w','c','c','c','w','c','c','w','w','w','c','w','c','c','w','w','w','c','c','c','c','c','w','w','w','w','w','w','w','w','w','w'];
+  const gameString1 = ['w','w','w','w','w','w','w','w','w','c','w','w','c','c','c','w','w','c','c','c','c','c','c','w','w','c','w','c','w','c','w','w','w','w','c','c','w','c','c','w','w','w','c','w','c','c','w','w','w','c','c','c','c','c','w','w','w','w','w','w','w','w','w','w'];
   const $li = $('.li');
   const $ul = $('.ul');
   const digger = $('<img id ="digger" src="images/digger.png" alt="digger image" />');
   const block = $('<img id ="block" src="images/block.png" alt="block image" />');
   const ruby = $('<img id ="ruby" src="images/ruby.png" alt="ruby image" />');
   const $playButton = $('#play-button');
+  const $resetButton = $('#reset-button');
   // const gridWidth = 8; //UPDATE WITH BIGGER GRID
-  const startSquare = 43; //UPDATE with new game?
+  const startSquare = 10; //UPDATE with new game?
   let $beginningSquare = null;
-  let score = 0;
+  let score1 = 0;
   let timer = 60;
   const $score = $('#score');
   const $timer = $('#timer');
@@ -29,13 +30,13 @@ $(() => {
 
   function init (){
     loadBoard();
-    score();
     moveDigger();
     $playButton.on('click', function() {
       $beginningSquare = $('#grid li:nth-child(' + startSquare + ')');
       $beginningSquare.append(digger);
       spawn();
     });
+    score();
     reset();
 
 
@@ -92,6 +93,7 @@ console.log(n);
         }
         e.preventDefault(); // prevent the default action (scroll / move caret)
       });
+
     }
 
     //SPAWN RANDOM OBJECT
@@ -101,13 +103,17 @@ console.log(n);
       const $randomLocation = $clear[$randomSpawn];
       console.log($randomLocation);
       $($randomLocation).addClass('ruby').append(ruby);
-      score();
     }
 
     //SCORE POINTS
     function score(){
-      $('.ruby.digger').addClass('NEW');
+      if (($('#grid').hasClass('.ruby')) && ($('#grid').hasClass('.digger')) && ($('#grid').hasClass('.c'))) {
+        console.log('SCORED!!!!!');
+      } else {
+        console.log('NO');
+      }
     }
+
 
     // SCOREBOARD
     function scoreboard(){
@@ -118,12 +124,13 @@ console.log(n);
 
     // RESET GAME
     function reset() {
-      score = 0;
-      timer = 60;
-
-      $score.html(score);
-      $timer.html(timer);
+      // const score = 0;
+      // const timer = 60;
+      $resetButton.on('click', function(){
+        location.reload();
+        // $score.html(score);
+        // $timer.html(timer);
+      });
     }
-
   }
 });
