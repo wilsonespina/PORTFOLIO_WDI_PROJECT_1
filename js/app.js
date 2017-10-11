f8c689bbba75102a98cae49fdc7376510e396f9e
wilsonespina <wilson.espina@gmail.com>
2017-10-11 14:32:47 +0100

    refactoring

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@ $(() => {
   const grid = ['b0','b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12','b13','b14','b15','b16','b17','b18','b19','b20','b21','b22','b23','b24','b25','b26','b27','b28','b29','b30','b31','b32','b33','b34','b35','b36','b37','b38','b39','b40','b41','b42','b43','b44','b45','b46','b47','b48','b49','b50','b51','b52','b53','b54','b55','b56','b57','b58','b59','b60','b61','b62','b63','b64','b65','b66','b67','b68','b69','b70','b71','b72','b73','b74','b75','b76','b77','b78','b79','b80','b81','b82','b83','b84','b85','b86','b87','b88','b89','b90','b91','b92','b93','b94','b95','b96','b97','b98','b99'];
 // gameString1 = 8 x 8 grid
   const gameString1 =
-  // ['c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c'];
+  ['c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','g'];
   // ['w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','g','g','w','c','s','c','c','c','g','g','w','w','w','w','w','w','g','g'];
-  ['c','c','c','c','c','c','c','c','c','c','w','w','c','w','c','w','w','c','c','c','c','c','c','w','w','c','w','c','w','c','w','w','w','w','c','c','w','c','c','w','w','w','c','w','c','c','g','g','w','c','s','c','c','c','g','g','c','c','c','c','c','c','c','c'];
+  // ['c','c','c','c','c','c','c','c','c','c','w','w','c','w','c','w','w','c','c','c','c','c','c','w','w','c','w','c','w','c','w','w','w','c','c','c','w','c','c','w','w','c','c','w','c','c','c','g','w','c','s','c','c','c','c','g','c','c','c','c','c','c','c','c'];
 
 // gameString2 = 9 x 9 grid
   const gameString2 = ['c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c','c'];
@@ -25,6 +25,7 @@ $(() => {
   let timer = 60;
   const $score = $('#score');
   const $timer = $('#timer');
+
 
   init();
 
@@ -167,29 +168,6 @@ $(() => {
 
         }
         e.preventDefault(); // prevent the default action (scroll / move caret)
-
-        //MOVE BLOCK
-        function moveBlock(){
-          console.log(n);
-          console.log(e.which);
-          if (e.which === 37 && (($('#grid li:nth-child(' + (n-1) + ')').attr('class')).includes('ruby')) && ($('#grid li:nth-child(' + (n-1) + ')')) !== 'w') { //LEFT
-            console.log('LEFT');
-            // $('#grid li:nth-child(' + n + ')').append(digger);
-            // $('#grid li:nth-child(' + (n+1) + ')').addClass('c');
-            // $('#grid li:nth-child(' + (n-1) + ')').addClass('ruby').append(ruby);
-          } else if  (e.which === 38) { //UP
-
-          } else if  (e.which === 39) { //RIGHT
-
-          } else if  (e.which === 40) { //DOWN
-
-          } else {
-            return;
-          }
-        }
-
-
-
       });
 
       //SCOREBOARD
