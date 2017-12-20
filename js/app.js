bf83a779d120eb8958868fdf0a2a6175878c670f
wilsonespina <wilson.espina@gmail.com>
2017-12-20 13:51:26 +0000

    moved images to AWS for deployment

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 $(() => {
   const grid = ['b0','b1','b2','b3','b4','b5','b6','b7','b8','b9','b10','b11','b12','b13','b14','b15','b16','b17','b18','b19','b20','b21','b22','b23','b24','b25','b26','b27','b28','b29','b30','b31','b32','b33','b34','b35','b36','b37','b38','b39','b40','b41','b42','b43','b44','b45','b46','b47','b48','b49','b50','b51','b52','b53','b54','b55','b56','b57','b58','b59','b60','b61','b62','b63','b64','b65','b66','b67','b68','b69','b70','b71','b72','b73','b74','b75','b76','b77','b78','b79','b80','b81','b82','b83','b84','b85','b86','b87','b88','b89','b90','b91','b92','b93','b94','b95','b96','b97','b98','b99'];
   const rubySquares1 = ['b18','b19','b22','b27','b28','b30','b29','b30','b34','b35','b38','b43','b50','b51','b52'];
-  const loseSquares1 = ['b18','b19','b22','b27','b28','b30','b29','b30','b34','b35','b38','b43','b50','b51','b52'];
   // gameString1 = 8 x 8 grid
   const gameString1 =
   ['w','w','w','w','w','w','w','w','w','s','c','c','c','c','c','w','w','c','c','c','w','w','c','w','w','c','w','c','c','c','c','w','w','c','c','c','c','w','c','w','w','c','w','c','w','w','c','w','w','c','c','c','c','c','g','w','w','w','w','w','w','w','w','w'];
@@ -12,9 +11,9 @@ $(() => {
   const $li = $('.li');
   const $ul = $('.ul');
   const $goal = $('.g');
-  const digger = $('<img id ="digger" src="images/diglett.png" alt="digger image" />');
-  const block = $('<img id ="block" src="images/block.png" alt="block image" />');
-  const ruby = $('<img id ="ruby" src="images/ball.png" alt="ruby image" />');
+  const digger = $('<img id ="digger" src="https://s3-eu-west-1.amazonaws.com/wdi-30-ldn/wdi-project-1/digger.png" alt="digger image" />');
+  const block = $('<img id ="block" src="https://s3-eu-west-1.amazonaws.com/wdi-30-ldn/wdi-project-1/block.png" alt="block image" />');
+  const ruby = $('<img id ="ruby" src="https://s3-eu-west-1.amazonaws.com/wdi-30-ldn/wdi-project-1/ruby.png" alt="ruby image" />');
   const $playButton = $('#play-button');
   const $resetButton = $('#reset-button');
   const gridWidth = Math.sqrt(gameString1.length); //UPDATE WITH BIGGER GRID
